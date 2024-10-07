d7cb9e26c45ec46e1941d3616b19961f8bb4cf70
sanjeevkdosso21 <sanjeev.dosso21@gmail.com>
2024-10-07 11:53:57 +0530

    s

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 
 // Routes
-app.use('/api/users', userRoutes);
+app.use('/api/user', userRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const { registerUser, loginUser } = require('../controllers/userController');
+const { createUser, loginUserCtrl } = require('../controllers/userController');
 const router = express.Router();
 
-router.post('/register', registerUser);
-router.post('/login', loginUser);
+router.post('/register', createUser);
+router.post('/login', loginUserCtrl);
 
 module.exports = router;

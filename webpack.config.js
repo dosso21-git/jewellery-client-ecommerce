cb0c4f657b24c6c6d6a0866383bd015d95a75260
vikramdosso21 <vikramdosso21@gmail.com>
2024-10-11 18:44:47 +0530

    AD

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+import path from 'path';
+
+export default {
+    entry: './src/index.jsx',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(process.cwd(), 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[path][name].[ext]',
+                    },
+                },
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+};
